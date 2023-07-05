60cbb5f1707870ac0466e42b24bba7bec23a7cca
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-05 10:17:38 -0500

    Change the position of the handleClick function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,8 +4,18 @@ import calculate from '../logic/calculate';
 
 // Child component
 function CalculatorButton({
-  className, text, id, handleClick,
+  className, text, id,
 }) {
+  const obj = useState({ total: '', next: '', operation: '' });
+
+  const handleClick = (event) => {
+    const operationResult = calculate(obj, event.target.textContent).total;
+    console.log(calculate(obj, event.target.textContent));
+    console.log(operationResult);
+    const result = document.getElementById('result');
+    result.textContent = operationResult;
+  };
+
   return (
     <button type="button" className={className} id={id} onClick={handleClick}>
       {text}
@@ -15,38 +25,29 @@ function CalculatorButton({
 
 // Main calculator component
 export default function Calculator() {
-  const obj = useState({ total: '', next: '', operation: '' });
-
-  const handleClick = (event) => {
-    console.log(event.target);
-    const operationResult = calculate(obj, event.target.value).total;
-    const result = document.getElementById('result');
-    result.textContent = operationResult;
-  };
-
   return (
     <div id="calculator-container">
       <div id="result">0</div>
       <div id="operations-container">
-        <CalculatorButton className="operations" text="AC" onClick={handleClick} />
-        <CalculatorButton className="operations" text="+/-" onClick={handleClick} />
-        <CalculatorButton className="operations" text="%" onClick={handleClick} />
-        <CalculatorButton className="operations right-operations" text="÷" onClick={handleClick} />
-        <CalculatorButton className="operations" text="7" onClick={handleClick} />
-        <CalculatorButton className="operations" text="8" onClick={handleClick} />
-        <CalculatorButton className="operations" text="9" onClick={handleClick} />
-        <CalculatorButton className="operations right-operations" text="x" onClick={handleClick} />
-        <CalculatorButton className="operations" text="4" onClick={handleClick} />
-        <CalculatorButton className="operations" text="5" onClick={handleClick} />
-        <CalculatorButton className="operations" text="6" onClick={handleClick} />
-        <CalculatorButton className="operations right-operations" text="-" onClick={handleClick} />
-        <CalculatorButton className="operations" text="1" onClick={handleClick} />
-        <CalculatorButton className="operations" text="2" onClick={handleClick} />
-        <CalculatorButton className="operations" text="3" onClick={handleClick} />
-        <CalculatorButton className="operations right-operations" text="+" onClick={handleClick} />
-        <CalculatorButton className="operations" id="zero-button" text="0" onClick={handleClick} />
-        <CalculatorButton className="operations" text="." onClick={handleClick} />
-        <CalculatorButton className="operations right-operations" text="=" onClick={handleClick} />
+        <CalculatorButton className="operations" text="AC" />
+        <CalculatorButton className="operations" text="+/-" />
+        <CalculatorButton className="operations" text="%" />
+        <CalculatorButton className="operations right-operations" text="÷" />
+        <CalculatorButton className="operations" text="7" />
+        <CalculatorButton className="operations" text="8" />
+        <CalculatorButton className="operations" text="9" />
+        <CalculatorButton className="operations right-operations" text="x" />
+        <CalculatorButton className="operations" text="4" />
+        <CalculatorButton className="operations" text="5" />
+        <CalculatorButton className="operations" text="6" />
+        <CalculatorButton className="operations right-operations" text="-" />
+        <CalculatorButton className="operations" text="1" />
+        <CalculatorButton className="operations" text="2" />
+        <CalculatorButton className="operations" text="3" />
+        <CalculatorButton className="operations right-operations" text="+" />
+        <CalculatorButton className="operations" id="zero-button" text="0" />
+        <CalculatorButton className="operations" text="." />
+        <CalculatorButton className="operations right-operations" text="=" />
       </div>
     </div>
   );
