2a58c4a42f749fd40b41a3b4f86b9df044663e02
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-05 14:42:40 -0500

    Import the component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,41 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-import calculate from '../logic/calculate';
-
-// Child component
-function CalculatorButton({
-  className, text, id, obj, setObj,
-}) {
-  const handleClick = (event) => {
-    const operation = calculate(obj, event.target.textContent);
-
-    setObj((oldObj) => ({
-      ...oldObj,
-      ...operation,
-    }));
-
-    const result = document.getElementById('result');
-    // If there's a zero, update the content.
-    if (result.textContent === '0') {
-      result.textContent = '';
-    }
-    // Concat the operations.
-    result.textContent += event.target.textContent;
-    // If the user press equal, display the result.
-    if (event.target.textContent === '=') {
-      result.textContent = '';
-      result.textContent = operation.total || '0';
-    } else if (event.target.textContent === 'AC') {
-      result.textContent = '0';
-    }
-  };
-
-  return (
-    <button type="button" className={className} id={id} onClick={handleClick}>
-      {text}
-    </button>
-  );
-}
+import CalculatorButton from './calculatorButton';
 
 // Main calculator component
 export default function Calculator() {
