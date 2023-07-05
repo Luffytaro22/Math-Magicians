6226aa6eb1bbf1de8aa8ec55467260fbee0303d9
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-05 14:28:04 -0500

    Modified the code to pass obj and setObj as props

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,17 +3,16 @@ import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
 // Child component
-function CalculatorButton({ className, text, id }) {
-  const [obj, setObj] = useState({ total: null, next: null, operation: null });
-
+function CalculatorButton({
+  className, text, id, obj, setObj,
+}) {
   const handleClick = (event) => {
     const operation = calculate(obj, event.target.textContent);
 
-    setObj({
-      total: operation.total,
-      next: operation.next,
-      operation: operation.operation,
-    });
+    setObj((oldObj) => ({
+      ...oldObj,
+      ...operation,
+    }));
 
     console.log(operation);
     console.log(obj);
@@ -30,29 +29,31 @@ function CalculatorButton({ className, text, id }) {
 
 // Main calculator component
 export default function Calculator() {
+  const [obj, setObj] = useState({ total: null, next: null, operation: null });
+
   return (
     <div id="calculator-container">
       <div id="result">0</div>
       <div id="operations-container">
-        <CalculatorButton className="operations" text="AC" />
-        <CalculatorButton className="operations" text="+/-" />
-        <CalculatorButton className="operations" text="%" />
-        <CalculatorButton className="operations right-operations" text="÷" />
-        <CalculatorButton className="operations" text="7" />
-        <CalculatorButton className="operations" text="8" />
-        <CalculatorButton className="operations" text="9" />
-        <CalculatorButton className="operations right-operations" text="x" />
-        <CalculatorButton className="operations" text="4" />
-        <CalculatorButton className="operations" text="5" />
-        <CalculatorButton className="operations" text="6" />
-        <CalculatorButton className="operations right-operations" text="-" />
-        <CalculatorButton className="operations" text="1" />
-        <CalculatorButton className="operations" text="2" />
-        <CalculatorButton className="operations" text="3" />
-        <CalculatorButton className="operations right-operations" text="+" />
-        <CalculatorButton className="operations" id="zero-button" text="0" />
-        <CalculatorButton className="operations" text="." />
-        <CalculatorButton className="operations right-operations" text="=" />
+        <CalculatorButton className="operations" text="AC" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations" text="+/-" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations" text="%" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations right-operations" text="÷" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations" text="7" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations" text="8" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations" text="9" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations right-operations" text="x" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations" text="4" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations" text="5" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations" text="6" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations right-operations" text="-" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations" text="1" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations" text="2" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations" text="3" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations right-operations" text="+" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations" id="zero-button" text="0" obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations" text="." obj={obj} setObj={setObj} />
+        <CalculatorButton className="operations right-operations" text="=" obj={obj} setObj={setObj} />
       </div>
     </div>
   );
