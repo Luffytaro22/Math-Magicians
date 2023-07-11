c1e4a6eabf3b0bb155319bf35aeecce7278d0fd8
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-11 10:40:55 -0500

    Created a NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import { NavLink } from 'react-router-dom';
+import React from 'react';
+
+const links = [
+  {
+    path: '/',
+    text: 'Home',
+  },
+  {
+    path: 'calculator',
+    text: 'Calculator',
+  },
+  {
+    poath: 'quotes',
+    text: 'Quotes',
+  },
+];
+
+export default function NavBar() {
+  return (
+    <header style={{
+      display: 'flex',
+      justifyContent: 'space-around',
+    }}
+    >
+      <h1>Math Magicians</h1>
+      <nav id="nav-bar">
+        <ul id="nav-ul">
+          {links.map((link) => (
+            <li className="ul-links" key={link.text}>
+              <NavLink to={link.path}>{link.text}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
