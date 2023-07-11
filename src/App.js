2d62023d4eafc184526906ab3623274741939e05
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-11 11:08:11 -0500

    Render the routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import Calculator from './components/Calculator';
-import APIRequest from './components/quoteAPI';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import CalculatorPage from './routes/Calculator';
+import Home from './routes/Home';
+import Quotes from './routes/Quotes';
+import ErrorPage from './routes/Error-page';
 
 function App() {
   return (
-    <>
-      <Calculator />
-      <APIRequest />
-    </>
+    <Routes>
+      <Route path="/" element={<NavBar />}>
+        <Route index element={<Home />} />
+        <Route path="calculator" element={<CalculatorPage />} />
+        <Route path="Quotes" element={<Quotes />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
   );
 }
 
