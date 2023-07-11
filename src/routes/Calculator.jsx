23a8252a8ba8fb13bfe31999546f6231dc90a86d
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-11 11:00:30 -0500

    Added the content of the calculator page

---
+++ src/routes/Calculator.jsx
@@ -0,0 +1,10 @@
+import Calculator from '../components/Calculator';
+
+export default function CalculatorPage() {
+  return (
+    <div id="calculator-div">
+      <h2>Let&aposs do some math!</h2>
+      <Calculator />
+    </div>
+  );
+}
